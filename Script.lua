fa6f3bb174470b4b14946c62691899d2682a82f7
bmplayzzz <88953759+bmplayzzz@users.noreply.github.com>
2021-08-14 21:40:48 -0400

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,22 @@
-print("Lua Obfuscator by Pyan")
+local RedPart = script.Parent.BluePart
+local BluePart = script.Parent.RedPart
+
+local function RedFunction()
+	RedPart.Transparency = 0
+	wait(.01)
+	RedPart.Transparency = 1
+end
+
+local function BlueFunction()
+	BluePart.Transparency = 0
+	wait(.01)
+	BluePart.Transparency = 1	
+end
+
+while true do 
+	BlueFunction()
+	wait(.01)
+	RedFunction()
+	wait(.01)	
+end
+
